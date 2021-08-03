c78743c9ad59bfa1d31e85500a5a30b0843b0411
ShishkoAleksander <87187345+ShishkoAleksander@users.noreply.github.com>
2021-08-03 13:48:30 +0300

    Update deploy2.sh

--- deploy2.sh
+++ deploy2.sh
@@ -13,12 +13,13 @@ mkdir mern
 cd mern
 git clone --branch master https://github.com/ShishkoAleksander/minikube.git
 cd minikube/
+echo "I'm waiting for 2 minutes while backend is delpoing"
 helm install backend server/chart
 sleep 2m
-echo "I'm waiting for 2 minutes while backend is delpoing"
+echo "I'm waiting for 2 minutes while frontend is delpoing"
 helm install frontend client/chart
 sleep 2m
-echo "I'm waiting for 2 minutes while frontend is delpoing"
 podname=$(kubectl get pods -o=name |  sed "s/^.\{4\}//" | grep front)
 kubectl port-forward --address 0.0.0.0 $podname 3000:3000
 echo "End job"
+echo "Good Luck and lets start to check service)"
