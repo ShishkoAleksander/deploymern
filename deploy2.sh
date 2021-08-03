8e06ce16744c485caeb56ee938f52fea37d3ee90
ShishkoAleksander <87187345+ShishkoAleksander@users.noreply.github.com>
2021-08-03 13:50:29 +0300

    Update deploy2.sh

--- deploy2.sh
+++ deploy2.sh
@@ -5,10 +5,10 @@ minikube start --extra-config=apiserver.service-node-port-range=1-65535
 sudo apt install snapd
 echo "Installing helm"
 sudo snap install helm --classic
+echo "I'm waiting for 2 minutes while mongo is delpoing"
 helm repo add bitnami https://charts.bitnami.com/bitnami
 helm install mongo --set usePassword=false,replicaSet.enabled=true,service.type=LoadBalancer,replicaSet.replicas.secondary=3 bitnami/mongodb
 sleep 2m
-echo "I'm waiting for 2 minutes while mongo is delpoing"
 mkdir mern
 cd mern
 git clone --branch master https://github.com/ShishkoAleksander/minikube.git
